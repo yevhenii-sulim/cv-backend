d745a8003d1a9dadbeb627bf5196d14c592b973e
yevhenii-sulim <bees127@ukr.net>
2026-06-02 14:26:07 +0300

    change yml

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/cv-front-end/',
+  base: '/cv-backend/',
   plugins: [react()],
 });
